921d7a76fdc8dfb27bafab5271fda4b0b9cc8360
Oleh <op663246@gmail.com>
2023-06-10 09:51:59 +0300

    test_bot

--- lesson7/bot.py
+++ lesson7/bot.py
@@ -1,47 +1,53 @@
+import logging
 from aiogram import types, Dispatcher, Bot, executor
-from inline import first
-from default import colors as c
-import asyncio
+from aiogram.dispatcher import FSMContext
+from aiogram.contrib.fsm_storage.memory import MemoryStorage
+from inline import first, social_networks 
+from default import colors, request_contact
 
+logging.basicConfig(level=logging.INFO)
 
-
-TOKEN = "5991419295:AAEYklQPfg2wTaEx8zXZfkrrwQnUOuWQsXI"
-
-bot = Bot(token=TOKEN)
-dp = Dispatcher(bot)
-
+bot = Bot('5991419295:AAEYklQPfg2wTaEx8zXZfkrrwQnUOuWQsXI')
+dp = Dispatcher(bot, storage=MemoryStorage())
 
 
 @dp.message_handler(commands='start')
 async def start(message: types.Message):
-    await message.answer(text = "Hello!")
+    await message.answer(text='Hello!')
 
 
-
-@dp.message_handler(content_types='text')
+@dp.message_handler(text='I wanna press button.')
 async def echo(message: types.Message):
     await message.answer(text='Press this button!', reply_markup=first)
 
-# @dp.callback_query_handler(text='first')
-# async def second_step(Callback: types.CallbackQuery):
-#     await Callback.message.answer('1**3')
 
-# @dp.callback_query_handler(text='second')
-# async def second_step(Callback: types.CallbackQuery):
-#     await Callback.message.answer('8**')
+@dp.callback_query_handler()
+async def second_step(callback: types.CallbackQuery):
+    await callback.message.answer('You\'ve already pressed.')
 
 
-# @dp.callback_query_handler(text='third')
-# async def second_step(Callback: types.CallbackQuery):
-#     await Callback.message.answer('27')
+@dp.message_handler(commands='choose_color')
+async def choose_color(message: types.Message):
+    await message.answer('Choose your color.', reply_markup=colors)
 
 
+@dp.message_handler(commands='give-info')
+async def get_contact(message: types.Message):
+    await message.answer('Gimme your information!', reply_markup=request_contact)
 
 
+@dp.message_handler(content_types=types.ContentTypes.CONTACT)
+async def use_contact_info(message: types.Message):
+    if message.contact.user_id != message.from_id:
+        await message.answer('That is not your number.')
+    else:
+        await message.answer(f'Hello, {message.contact.phone_number}')
 
 
+@dp.message_handler(commands='socials')
+async def get_socials(message: types.Message):
+    await message.answer('Available socials:', reply_markup=social_networks)
 
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     executor.start_polling(dp)
--- lesson7/default.py
+++ lesson7/default.py
@@ -3,21 +3,22 @@ from aiogram.types import ReplyKeyboardMarkup, KeyboardButton
 colors = ReplyKeyboardMarkup(
     keyboard=[
         [
-            KeyboardButton(text = 'Red')
-        ],
-        [
-            KeyboardButton(text = 'Blue')
-        ],
-        [
-            KeyboardButton(text = 'Black')
+            KeyboardButton(text='🔴Red'),
+            KeyboardButton(text='💚Green'),
+            KeyboardButton(text='🟦Blue')
         ]
-    ]
+    ],
+    one_time_keyboard=True,
+    resize_keyboard=True
 )
 
 
 request_contact = ReplyKeyboardMarkup(
     keyboard=[
-        [KeyboardButton(text='Share my contact.', request_contact=True)]
+        [
+            KeyboardButton(text='Share my contact.', request_contact=True),
+            KeyboardButton(text='Share my location.', request_location=True)
+        ]
     ],
     resize_keyboard=True,
     one_time_keyboard=True
