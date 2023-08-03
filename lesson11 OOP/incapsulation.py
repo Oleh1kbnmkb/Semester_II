e6ba08c802d8b93a8bf89f1c0b3e7c27f55f5fef
Oleh <op663246@gmail.com>
2023-08-03 22:50:52 +0300

    OOP

--- lesson11 OOP/incapsulation.py
+++ lesson11 OOP/incapsulation.py
@@ -1,27 +1,26 @@
 class MyClass:
-  def __init__(self):
-    self.public_attribute = "Public attribute"
-    self.__private_attribute = "Private attribute"
-    self._protected_attribute = "Protected attribute "
+    def __init__(self):
+        self.public_attribute = "Public attribute"     #  Доступ із будь-якої точки
+        self.__private_attribute = "Private attribute"   # Доступ заборонений із будь-якої точки
+        self._protected_attribute = "Protected attribute"   # Доступ у класі (між методами)
+
+
+    def public_method(self):
+        print('This is public method')
+
     
-  
-  def public_method(self):
-    print('This is public method')
-    
-    
-    
-  def _private_method(self):
-    print('This is private method')
-    
-  
-  def _protected_method(self):
-    print('This is protected method')
-    
-    
-    
+    def __private_method(self):
+        print('This is private method')
+
+
+    def _protected_method(self):
+        print('This is protected method')
+
+
 obj = MyClass()
 print(obj.public_attribute)
 obj.public_method()
 
+
 print(obj._protected_attribute)
-obj._private_method()
+obj._protected_method()
