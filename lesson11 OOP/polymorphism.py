c31cae7dd39c179107091fb8610e609565d64f60
Oleh <op663246@gmail.com>
2023-08-05 09:07:36 +0300

    OOP

--- lesson11 OOP/polymorphism.py
+++ lesson11 OOP/polymorphism.py
@@ -40,4 +40,50 @@ print(r.calc_area())
 
 c = Circle(4)
 print(c.calc_area())
+
+
+
+
+
+
+
+class Shape:
+    def __init__(self) -> None:
+        self.perimeter = 0
+
+    def calc_perimeter(self):
+        print('It returns perimeter')
+
+
+class Rectangle(Shape):
+    def __init__(self, a, b) -> None:
+        super().__init__()
+        self.a = a
+        self.b = b
+
+    def calc_perimeter(self):
+        self.perimeter = 2 * (self.a + self.b)
+        return self.perimeter
+
+
+class Circle(Shape):
+    def __init__(self, radius) -> None:
+        super().__init__()
+        self.r = radius
+
+    def calc_perimeter(self):
+        self.perimeter = 2 * 3.14 * self.r
+        return self.perimeter
+
+
+s = Shape()
+s.calc_perimeter()
+
+r = Rectangle(2, 5)
+print(r.calc_perimeter())
+
+c = Circle(4)
+print(c.calc_perimeter())
+
+
         
